839eed4c37295b3a6e1739413059dc506c363a5a
DarkestEcho <60607354+DarkestEcho@users.noreply.github.com>
2020-06-01 13:58:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace лб5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int number = 24;
+            var a = await GetArrayAsync(number);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static async Task<int[]> GetArrayAsync(int n)
+        {
+            int[] resArray = await Task.Run(()=> GetArray(n));
+
+            return resArray;
+        }
+
+        static int[] GetArray(int n)
+        {
+            int[] newArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                newArray[i]=i;
+            }
+            return newArray;
+        }
+    }
+}
